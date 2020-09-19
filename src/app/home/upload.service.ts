2e4b9c4e441d41c806ee80faa1e943f23183ebc1
dev-PC\dev <nadeera3784@gmail.com>
2020-09-18 23:50:00 -0700

    feat : upload service file added

---
+++ src/app/home/upload.service.ts
@@ -0,0 +1,22 @@
+import {HttpClient, HttpEvent, HttpErrorResponse, HttpEventType} from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+    providedIn: 'root'
+})
+
+export class UploadService { 
+
+    base_url: string = 'http://localhost:3000';
+
+    constructor(private httpClient: HttpClient) { }
+    
+    public upload(formData) {
+        return this.httpClient.post<any>(this.base_url+'/api/upload', formData, {
+              reportProgress: true,
+              observe: 'events'
+        });
+    }         
+}
+    
+
