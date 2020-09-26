702e0cc2f0d6ef1189f80628ea917cdc3da82b45
dev-PC\dev <nadeera3784@gmail.com>
2020-09-26 01:54:03 -0700

    feat : s3 file upload function added

--- server.js
+++ server.js
@@ -2,34 +2,61 @@ const path = require('path');
 const fs = require('fs');
 const express = require('express');
 const multer = require('multer');
+const multerS3 = require('multer-s3');
 const bodyParser = require('body-parser')
 const app = express();
 const router = express.Router();
+//const upload = require("./uploadMiddleware.js"); 
+const AWS = require('aws-sdk');
 
-const DIR = './uploads';
+const BUCKET_NAME = "epanthiyabucket";
 
-
-let storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-      cb(null, DIR);
-    },
-    filename: (req, file, cb) => {
-      cb(null, file.fieldname + '-' + Date.now() + '.' + path.extname(file.originalname));
-    }
+AWS.config.update({
+  accessKeyId: "AKIA4R3ZC4QQGHSHEQ37",
+  secretAccessKey: "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",
+  region: 'ap-south-1' 
 });
-let upload = multer({
-    storage: storage,
-    fileFilter: function(req, file, cb) {
-        if (file.mimetype !== 'image/png' && file.mimetype !== 'image/gif' && file.mimetype !== 'image/jpeg') {
-            //req.fileValidationError = 'Only image files are allowed!';
-            return cb('Only image files are allowed!', false);
-            cb(null, false);
-        } else {
-            cb(null, true);
-        }
-    },
-    limits: { fileSize: 1024 * 1024 },
+
+s3 = new AWS.S3({apiVersion: '2006-03-01'});
+
+var upload = multer({
+  storage: multerS3({
+      s3: s3,
+      acl: 'private',
+      bucket: BUCKET_NAME,
+      metadata: function (req, file, cb) {
+        cb(null, { fieldName: file.fieldname });
+      },
+      key: function (req, file, cb) {
+          console.log(file);
+          cb(null, file.originalname); //use Date.now() for unique file keys
+      }
+  })
 });
+// const DIR = './uploads';
+
+
+// let storage = multer.diskStorage({
+//     destination: (req, file, cb) => {
+//       cb(null, DIR);
+//     },
+//     filename: (req, file, cb) => {
+//       cb(null, file.fieldname + '-' + Date.now() + '.' + path.extname(file.originalname));
+//     }
+// });
+// let upload = multer({
+//     storage: storage,
+//     fileFilter: function(req, file, cb) {
+//         if (file.mimetype !== 'image/png' && file.mimetype !== 'image/gif' && file.mimetype !== 'image/jpeg') {
+//             //req.fileValidationError = 'Only image files are allowed!';
+//             return cb('Only image files are allowed!', false);
+//             cb(null, false);
+//         } else {
+//             cb(null, true);
+//         }
+//     },
+//     limits: { fileSize: 1024 * 1024 },
+// });
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
@@ -57,11 +84,35 @@ app.post('/api/upload', upload.single('attachment'), function (req, res) {
         });
     
       } else {
+        const mimeType = req.file.mimetype;
+        const file_name = req.file.originalname;
         console.log('file received successfully');
         return res.send({
           data : req.file,
           success: true
-        })
+        });
+
+        // const s3Params = {
+        //   Bucket: "epanthiyabucket",
+        //   Body: file.data,
+        //   Key: file_name,
+        //   Expires: 60,
+        //   ContentType: mimeType,
+        //   ACL: 'public-read'
+        // };
+        // s3.upload('putObject', s3Params, (err, data) => {
+        //   if(err){
+        //     console.log(err);
+        //     return res.end();
+        //   }
+        //   const returnData = {
+        //     signedRequest: data,
+        //     url: `https://epanthiyabucket.s3.amazonaws.com/file_name`
+        //   };
+        //   res.write(JSON.stringify(returnData));
+        //   res.end();
+        // });
+
       }
 });
  
